c3662a0b7acc73db832de2ec85ab0f9c6c6f9d16
alimdzhatdoev <141133089+alimdzhatdoev@users.noreply.github.com>
2023-10-11 12:17:18 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.mySwiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
